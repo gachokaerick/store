5b76fdf62096175cc1deceb570539970f605c7c1
Erick Gachoka <gachokaeric819@gmail.com>
2022-03-14 05:58:22 +0300

    adds tabs on checkout for address and cart sections

--- src/main/webapp/app/modules/cart/cart.tsx
+++ src/main/webapp/app/modules/cart/cart.tsx
@@ -10,6 +10,9 @@ import { isIdPresent } from 'app/shared/util/entity-utils';
 import { getLoginUrl, REDIRECT_URL } from 'app/shared/util/url-utils';
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import PaypalCheckout from 'app/modules/checkout/paypal-checkout';
+import { Tabs } from 'antd';
+import { StickyContainer, Sticky } from 'react-sticky';
+import './cart.scss';
 
 export const Cart = (props: RouteComponentProps<{ url: string }>) => {
   const dispatch = useAppDispatch();
@@ -18,6 +21,7 @@ export const Cart = (props: RouteComponentProps<{ url: string }>) => {
   const catalogItems = useAppSelector(state => state.catalogItem.entities);
   const isAuthenticated = useAppSelector(state => state.authentication.isAuthenticated);
   const [{ isPending }] = usePayPalScriptReducer();
+  const { TabPane } = Tabs;
 
   useEffect(() => {
     if (cart.length > 0 && catalogItems.length !== cart.length) {
@@ -50,6 +54,14 @@ export const Cart = (props: RouteComponentProps<{ url: string }>) => {
       return catalogItem ? total + catalogItem.price * item.quantity : total + item.price * item.quantity;
     }, 0);
 
+  const renderTabBar = (barProps, DefaultTabBar) => (
+    <Sticky bottomOffset={80}>
+      {({ style, isSticky }) => (
+        <DefaultTabBar {...barProps} className="site-custom-tab-bar" style={{ ...style, top: isSticky ? `${62 + style.top}px` : `0px` }} />
+      )}
+    </Sticky>
+  );
+
   return (
     <Row
       gutter={[
@@ -58,92 +70,105 @@ export const Cart = (props: RouteComponentProps<{ url: string }>) => {
       ]}
     >
       <Col lg={16} xs={24}>
-        <Card title={`Cart (${cart.length})`} bordered={true}>
-          {catalogItems
-            ? catalogItems
-                .filter(item => isIdPresent(cart, item.id))
-                .map((item, i, { length }) => (
-                  <Space key={item.id} direction={'vertical'} className={'w-100'}>
-                    <Row>
-                      <Col xs={24} sm={6}>
-                        <Image src={item.pictureUrl} height={100} />
-                      </Col>
-                      <Col xs={24} sm={18}>
+        <StickyContainer>
+          <Tabs defaultActiveKey="1" size={'large'} renderTabBar={renderTabBar}>
+            <TabPane tab={`Cart (${cart.length})`} key="1">
+              {catalogItems
+                ? catalogItems
+                    .filter(item => isIdPresent(cart, item.id))
+                    .map((item, i, { length }) => (
+                      <Space key={item.id} direction={'vertical'} className={'w-100'}>
                         <Row>
-                          <Col xs={24} md={12}>
-                            <Title level={5}>Name</Title>
-                            <Text className={'d-block'}>{item.description}</Text>
-                            <Text className={'d-block'}>Available: {item.availableStock}</Text>
+                          <Col xs={24} sm={6}>
+                            <Image src={item.pictureUrl} height={100} />
                           </Col>
-                          <Col xs={24} md={12} className={'text-md-right'}>
-                            <Title level={4}>${item.price}</Title>
+                          <Col xs={24} sm={18}>
+                            <Row>
+                              <Col xs={24} md={12}>
+                                <Title level={5}>Name</Title>
+                                <Text className={'d-block'}>{item.description}</Text>
+                                <Text className={'d-block'}>Available: {item.availableStock}</Text>
+                              </Col>
+                              <Col xs={24} md={12} className={'text-md-right'}>
+                                <Title level={4}>${item.price}</Title>
+                              </Col>
+                            </Row>
                           </Col>
                         </Row>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col span={12}>
-                        <Button type={'text'} icon={<DeleteOutlined />} danger onClick={() => dispatch(removeItemFromCart(item))}>
-                          Remove
-                        </Button>
-                      </Col>
-                      <Col span={12} className={'text-right'}>
-                        <Space direction={'horizontal'}>
-                          <Button type={'primary'} icon={<PlusOutlined />} onClick={() => dispatch(addItemToCart(item))} />
-                          <Text>{getItemFromCartById(item.id)?.quantity}</Text>
-                          <Button type={'primary'} icon={<MinusOutlined />} onClick={() => dispatch(reduceFromCart(item))} />
-                        </Space>
-                      </Col>
-                    </Row>
-                    <Divider className={length - 1 === i ? 'd-none' : ''} />
-                  </Space>
-                ))
-            : null}
-        </Card>
+                        <Row>
+                          <Col span={12}>
+                            <Button type={'text'} icon={<DeleteOutlined />} danger onClick={() => dispatch(removeItemFromCart(item))}>
+                              Remove
+                            </Button>
+                          </Col>
+                          <Col span={12} className={'text-right'}>
+                            <Space direction={'horizontal'}>
+                              <Button type={'primary'} icon={<PlusOutlined />} onClick={() => dispatch(addItemToCart(item))} />
+                              <Text>{getItemFromCartById(item.id)?.quantity}</Text>
+                              <Button type={'primary'} icon={<MinusOutlined />} onClick={() => dispatch(reduceFromCart(item))} />
+                            </Space>
+                          </Col>
+                        </Row>
+                        <Divider className={length - 1 === i ? 'd-none' : ''} />
+                      </Space>
+                    ))
+                : null}
+            </TabPane>
+            <TabPane tab="Address" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+          </Tabs>
+        </StickyContainer>
       </Col>
       <Col lg={8} xs={24}>
-        <Card title={`Cart Summary`} bordered={true}>
-          <Space direction={'vertical'} className={'w-100'}>
-            <Row>
-              <Col span={12}>
-                <Text>Subtotal</Text>
-              </Col>
-              <Col span={12} className={'text-md-right'}>
-                <Title level={4}>${getCheckoutTotal()}</Title>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <Text>Discount</Text>
-              </Col>
-              <Col span={12} className={'text-md-right'}>
-                <Text>$0</Text>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <Text>Total</Text>
-              </Col>
-              <Col span={12} className={'text-md-right'}>
-                <Title level={4}>${getCheckoutTotal()}</Title>
-              </Col>
-            </Row>
-            {!isAuthenticated ? (
-              <Button type={'primary'} className={'font-weight-bold'} block onClick={() => props.history.push(getLoginUrl())}>
-                CHECKOUT (${getCheckoutTotal()})
-              </Button>
-            ) : (
-              <>
-                {isPending ? <Spin /> : null}
-                <PaypalCheckout
-                  checkoutTotal={getCheckoutTotal()}
-                  catalogItems={catalogItems.filter(item => isIdPresent(cart, item.id))}
-                  getItemFromCartById={id => getItemFromCartById(id)}
-                />
-              </>
+        <StickyContainer className={'h-100'}>
+          <Sticky topOffset={0} bottomOffset={80}>
+            {({ style, isSticky }) => (
+              <Card title={`Cart Summary`} bordered={true} style={{ ...style, top: isSticky ? `${62 + style.top}px` : `0px` }}>
+                <Space direction={'vertical'} className={'w-100'}>
+                  <Row>
+                    <Col span={12}>
+                      <Text>Subtotal</Text>
+                    </Col>
+                    <Col span={12} className={'text-md-right'}>
+                      <Title level={4}>${getCheckoutTotal()}</Title>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={12}>
+                      <Text>Discount</Text>
+                    </Col>
+                    <Col span={12} className={'text-md-right'}>
+                      <Text>$0</Text>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={12}>
+                      <Text>Total</Text>
+                    </Col>
+                    <Col span={12} className={'text-md-right'}>
+                      <Title level={4}>${getCheckoutTotal()}</Title>
+                    </Col>
+                  </Row>
+                  {!isAuthenticated ? (
+                    <Button type={'primary'} className={'font-weight-bold'} block onClick={() => props.history.push(getLoginUrl())}>
+                      CHECKOUT (${getCheckoutTotal()})
+                    </Button>
+                  ) : (
+                    <>
+                      {isPending ? <Spin /> : null}
+                      <PaypalCheckout
+                        checkoutTotal={getCheckoutTotal()}
+                        catalogItems={catalogItems.filter(item => isIdPresent(cart, item.id))}
+                        getItemFromCartById={id => getItemFromCartById(id)}
+                      />
+                    </>
+                  )}
+                </Space>
+              </Card>
             )}
-          </Space>
-        </Card>
+          </Sticky>
+        </StickyContainer>
       </Col>
     </Row>
   );
--- src/main/webapp/app/modules/checkout/paypal-checkout.tsx
+++ src/main/webapp/app/modules/checkout/paypal-checkout.tsx
@@ -1,20 +1,70 @@
 import React from 'react';
 import { PayPalButtons } from '@paypal/react-paypal-js';
 import { ICatalogItem } from 'app/shared/model/catalog/catalog-item.model';
-import { PurchaseItem } from '@paypal/paypal-js/types/apis/orders';
+import { OrderResponseBody, PurchaseItem } from '@paypal/paypal-js/types/apis/orders';
+import { IPayment } from 'app/shared/model/orders/payment.model';
+import { OrderStatus } from 'app/shared/model/enumerations/order-status.model';
+import { IOrder } from 'app/shared/model/orders/order.model';
 
 export const PaypalCheckout = props => {
   const catalogItems: ReadonlyArray<ICatalogItem> = props.catalogItems;
+  const currencyCode = 'USD';
 
   const checkoutItems: PurchaseItem[] = catalogItems.map(item => ({
     name: `${item.name}`,
     quantity: `${props.getItemFromCartById(item.id).quantity}`,
     category: 'DIGITAL_GOODS',
     unit_amount: {
-      currency_code: 'USD',
+      currency_code: currencyCode,
       value: `${item.price}`,
     },
   }));
+
+  const makePayment = (paymentDetails: OrderResponseBody) => {};
+
+  const createOrderEntity = (paymentDetails: OrderResponseBody): IOrder => {
+    // return {
+    //   createTime: paymentDetails.create_time,
+    //   updateTime: paymentDetails.update_time,
+    //   paymentStatus: paymentDetails.status,
+    //   payerCountryCode: paymentDetails.payer.address.country_code,
+    //   payerEmail: paymentDetails.payer.email_address,
+    //   payerName: paymentDetails.payer.name.given_name,
+    //   payerSurname: paymentDetails.payer.name.surname,
+    //   payerId: paymentDetails.payer.payer_id,
+    //   currency: currencyCode,
+    //   amount: calculatePaymentAmount(paymentDetails),
+    //   paymentId: paymentDetails.id,
+    //   order: {
+    //     orderDate: paymentDetails.create_time,
+    //     orderStatus: OrderStatus.DRAFT,
+    //     description:
+    //   }
+    // };
+    return {
+      orderDate: paymentDetails.create_time,
+      orderStatus: OrderStatus.DRAFT,
+      description: paymentDetails.id + ': ' + paymentDetails.status,
+      address: {
+        // set address b4 paying for order
+      },
+      orderItems: [],
+      payments: [],
+      buyer: {},
+    };
+  };
+
+  const calculatePaymentAmount = (paymentDetails: OrderResponseBody): number => {
+    let total = 0;
+    paymentDetails.purchase_units.forEach(
+      unit =>
+        (total += unit.payments.captures.reduce(
+          (amount, capture) => (capture.status === 'COMPLETED' ? amount + Number((capture.amount as any).value) : 0),
+          0
+        ))
+    );
+    return total;
+  };
 
   return (
     <PayPalButtons
@@ -23,11 +73,11 @@ export const PaypalCheckout = props => {
           purchase_units: [
             {
               amount: {
-                currency_code: 'USD',
+                currency_code: currencyCode,
                 value: `${props.checkoutTotal}`,
                 breakdown: {
                   item_total: {
-                    currency_code: 'USD',
+                    currency_code: currencyCode,
                     value: `${props.checkoutTotal}`,
                   },
                 },
@@ -39,8 +89,7 @@ export const PaypalCheckout = props => {
       }}
       onApprove={(data, actions) => {
         return actions.order.capture().then(details => {
-          // eslint-disable-next-line no-console
-          console.log('details: ', details);
+          makePayment(details);
         });
       }}
     />
